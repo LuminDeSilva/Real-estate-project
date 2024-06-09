635543b53a08400d2232f31eb1a77fb5ad15e5bd
LuminDeSilva <luminde9958@gmail.com>
2024-06-09 23:33:40 +0530

    Create about page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function about() {
+export default function About() {
   return (
-    <div>about</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Lusan Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        Lusan Estate: Your Premier Destination for Real Estate Excellence. Discover the pinnacle of property services with our seasoned agents, committed to navigating you through seamless transactions in the most coveted neighborhoods. Whether you're buying, selling, or renting, trust in our expertise to elevate your real estate journey to unparalleled success.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      At Lusan Estate, Our Mission is Your Real Estate Success. With a focus on expert guidance, tailored service, and an intimate knowledge of the local landscape, we're dedicated to empowering you to achieve your property aspirations. Whether it's buying, selling, or renting, count on us to be your steadfast partner throughout every phase of your journey.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      At Lusan Estate, Experience Meets Excellence. Our seasoned team of agents brings a wealth of industry knowledge and a commitment to delivering top-tier service to our valued clients. We firmly believe that the journey of buying or selling a property should be nothing short of exhilarating and fulfilling. With unwavering dedication, we strive to transform this belief into reality for each and every client we serve.
+      </p>
+    </div>
   )
 }
